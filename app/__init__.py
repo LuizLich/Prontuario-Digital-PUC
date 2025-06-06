f44e90e4e52810481d049bca0c876dc40ae4c119
Luiz Felipe <luiz.terrasilva22@gmail.com>
2025-06-06 11:26:45 -0300

    render_test

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,25 @@
-from flask import Flask, url_for, flash, redirect
+import os
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import pymysql
-from app.views import main
 from flask_migrate import Migrate
-
-pymysql.install_as_MySQLdb()
 
 def create_app():
     app = Flask(__name__)
-    #app.config['SECRET_KEY'] = ''
-    conexao = 'mysql://root@localhost/db_fisio'
-    app.config['SQLALCHEMY_DATABASE_URI'] = conexao
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/db_fisio'
+
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+    if DATABASE_URL:
+        # Corrige a URL do Render para funcionar com SQLAlchemy
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    else:
+        # Manter conexão local
+        DATABASE_URL = 'mysql+pymysql://root@localhost/db_fisio'
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    from app.database import db
     
+    from app.database import db
     db.init_app(app)
+    
     migrate = Migrate(app, db)
 
     from app.views import main as main_blueprint
